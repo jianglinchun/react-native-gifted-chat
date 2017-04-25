4b15b5a8cb4b68a2fcc24567e62fa88ff2126e8e
jianglinchun <jianglinchun@live.com>
2017-04-25 10:25:47 +0800

    initial MessageFile(pdf/word/excel/ppt/zip/rar……)

---
+++ src/MessageFile.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import {
+  Image,
+  StyleSheet,
+  View,
+  Dimensions,
+} from 'react-native';
+
+export default class MessageFile extends React.Component {
+  render() {
+    const { width, height } = Dimensions.get('window');
+
+    return (
+      <View style={[styles.container, this.props.containerStyle]}>
+        <Text>Audio</Text>
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+  },
+  file: {
+    width: 150,
+    height: 100,
+    borderRadius: 13,
+    margin: 3,
+    resizeMode: 'cover',
+  }
+});
+
+MessageFile.defaultProps = {
+  currentMessage: {
+    file: null,
+  },
+  containerStyle: {},
+  fileStyle: {},
+  fileProps: {}
+};
+
+MessageFile.propTypes = {
+  currentMessage: React.PropTypes.object,
+  containerStyle: View.propTypes.style,
+  fileStyle: Image.propTypes.style,
+  fileProps: React.PropTypes.object
+};
